15d405061c4116abf22c2f23f7db48e9f3dad8c3
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-05-13 10:38:14 +0530

    GPS Tracker, dependencies set

---
+++ GPS_Tracking/app/src/main/java/com/example/gps_tracking/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.gps_tracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int DEFAULT_UPDATE_INTERVAL = 30;
+    public static final int FAST_UPDATE_INTERVAL = 5;
+    //  references to the UI elements
+    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
+
+    Switch sw_locationsupdates, sw_gps;
+
+//  variable to remember if we are tracking location or not
+    LocationRequest locationRequest;
+
+    boolean updateOn = false;
+
+//  location request is a config file for all setting related to FusedLocationProviderClient
+
+//  Google's API for location services. The majority of the app functions using this class
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//      give each UI variable a value
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_lon = findViewById(R.id.tv_lon);
+        tv_altitude = findViewById(R.id.tv_altitude);
+        tv_accuracy = findViewById(R.id.tv_accuracy);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_sensor = findViewById(R.id.tv_sensor);
+        tv_updates = findViewById(R.id.tv_updates);
+        tv_address = findViewById(R.id.tv_address);
+        sw_gps = findViewById(R.id.sw_gps);
+        sw_locationsupdates = findViewById(R.id.sw_locationsupdates);
+
+//  set all properties of LocationRequest
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(1000 * DEFAULT_UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(1000 * FAST_UPDATE_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY); 
+
+
+
+    }
+}
